34a5f780650d5431db9e27e3a4a2ca99592c73c5
kyomu <torimitsu.s.aa@m.titech.ac.jp>
2024-06-14 22:52:42 +0900

    カードの追加

--- src/App.vue
+++ src/App.vue
@@ -7,29 +7,26 @@ const count = ref(0)
 <template>
   <v-app>
     <v-main>
-      <div class="d-flex flex-column align-center">
-        <v-btn class="ma-2" color="primary" @click="count++">+1</v-btn>
-        <v-btn class="ma-2" color="secondary" @click="count--">-1</v-btn>
-        <div class="text-h4">count = {{ count }}</div>
-      </div>
+      <v-container class="d-flex flex-column align-center">
+          <v-card class="pa-4" elevation="2" max-width="400">
+          <div class="text-h4">count = {{ count }}</div>
+          <v-row>
+            <v-col cols="auto">
+              <v-btn variant="tonal" color="primary" @click="count++"> +1 </v-btn>
+            </v-col>
+            <v-col cols="auto">
+              <v-btn variant="tonal" color="secondary" @click="count--"> -1 </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <style scoped>
-.d-flex {
-  display: flex;
-}
-.flex-column {
-  flex-direction: column;
-}
-.align-center {
-  align-items: center;
-}
-.ma-2 {
-  margin: 8px;
-}
 .text-h4 {
   font-size: 1.5rem;
+  margin-bottom: 20px; /* テキストとボタンの間にスペースを追加 */
 }
 </style>
