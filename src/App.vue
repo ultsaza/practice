c167fa097a11b155528cb4e8ecf9d4937fd4b73c
kyomu <torimitsu.s.aa@m.titech.ac.jp>
2024-06-14 22:26:30 +0900

    vuetify によるボタンの作成

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,31 @@ const count = ref(0)
 </script>
 
 <template>
-  <main :class="$style.main">
-    <div>
-      <button :class="$style.button" @click="count++">+1</button>
-      <button :class="$style.button" @click="count--">-1</button>
-    </div>
-    <div>count = {{ count }}</div>
-  </main>
+  <v-app>
+    <v-main>
+      <div class="d-flex flex-column align-center">
+        <v-btn class="ma-2" color="primary" @click="count++">+1</v-btn>
+        <v-btn class="ma-2" color="secondary" @click="count--">-1</v-btn>
+        <div class="text-h4">count = {{ count }}</div>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
-<style module>
-.main {
+<style scoped>
+.d-flex {
   display: flex;
-  flex-direction: column;
-  font-size: 20px;
 }
-
-.button {
-  font-size: 20px;
+.flex-column {
+  flex-direction: column;
+}
+.align-center {
+  align-items: center;
+}
+.ma-2 {
+  margin: 8px;
+}
+.text-h4 {
+  font-size: 1.5rem;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-  components,
-  directives
-})
+import vuetify from './plugins/vuetify' // ここでVuetifyのプラグインをインポートします
 
 createApp(App).use(vuetify).mount('#app')
